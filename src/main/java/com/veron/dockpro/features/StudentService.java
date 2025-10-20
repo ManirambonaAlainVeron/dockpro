e24679e7f4d087e011e579c91d2a17e6457a3e43
Alain Véron MANIRAMBONA <alainveron.manirambona@obr.gov.bi>
2025-10-20 16:40:30 +0200

    setting networking

--- src/main/java/com/veron/dockpro/features/StudentService.java
+++ src/main/java/com/veron/dockpro/features/StudentService.java
@@ -2,15 +2,18 @@ package com.veron.dockpro.features;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
     
 
-    @Autowired
-    private StudentRepository studentRepository;
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> getAllStudents(){
         return studentRepository.findAll();
