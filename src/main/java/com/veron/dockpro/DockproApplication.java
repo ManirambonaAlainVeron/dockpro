6e4a15e298e67c540dc3eea7df5cf5cddd2d90a3
Alain Véron MANIRAMBONA <alainveron.manirambona@obr.gov.bi>
2025-10-23 17:53:35 +0200

    change my env variable

--- src/main/java/com/veron/dockpro/DockproApplication.java
+++ src/main/java/com/veron/dockpro/DockproApplication.java
@@ -3,10 +3,20 @@ package com.veron.dockpro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DockproApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+        // Apply them to the environment
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+		
 		SpringApplication.run(DockproApplication.class, args);
 	}
 
